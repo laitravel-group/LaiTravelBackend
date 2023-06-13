36684513e3eff5a8a68f18ec91c43fafc91ac285
stop-x13 <59541513+stop-x13@users.noreply.github.com>
2023-06-13 19:32:21 +1000

    Add template files

---
+++ src/main/java/com/laitravel/laitravelbe/LaitravelbeApplication.java
@@ -0,0 +1,13 @@
+package com.laitravel.laitravelbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaitravelbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaitravelbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laitravel/laitravelbe/LaitravelbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laitravel.laitravelbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaitravelbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
