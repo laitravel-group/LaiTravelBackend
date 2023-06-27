4dc9f7a2f16d8f6e8ade7df574650c4fc704204c
andyxdliu0218 <andyxdliu0218@gmail.com>
2023-06-26 20:14:28 -0700

    Update PlaceController.java

--- src/main/java/com/laitravel/laitravelbe/placeandinfomanagement/PlaceController.java
+++ src/main/java/com/laitravel/laitravelbe/placeandinfomanagement/PlaceController.java
@@ -15,15 +15,13 @@ public class PlaceController {
         this.placeService = placeService;
     }
 
-    @GetMapping("/getPlaces")
+    @GetMapping("/Places")
     public List<Place> getPlaces(@RequestParam(value="city") String city, @RequestParam(value="start") String startDate, @RequestParam(value="end") String endDate) {
         List<Place>results = placeService.placeSearch(city,startDate,endDate);
         System.out.println(results.size());
         return results;
 
     }
-
-
 
 
 
