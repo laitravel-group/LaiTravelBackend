268ea3e3cb5c1654449d6787c7d956e65fd68e61
stop-x13 <stop.x13@gmail.com>
2023-07-05 19:05:38 +1000

    More refactoring and better string concat

--- src/main/java/com/laitravel/laitravelbe/api/GoogleCloudService.java
+++ src/main/java/com/laitravel/laitravelbe/api/GoogleCloudService.java
@@ -4,49 +4,49 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 import com.google.cloud.storage.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GoogleCloudService {
     final Storage storage;
+    private final Logger LOGGER = LoggerFactory.getLogger(GoogleCloudService.class);
 
     public GoogleCloudService(Storage storage) {
         this.storage = storage;
     }
 
-    public String uploadImage(
-             String bucketName, String placeID, byte[] contents) {
-        if(contents == null) return "";
-        placeID = placeID + ".jpg";
-        BlobId blobId = BlobId.of(bucketName, placeID);
+    public String uploadImage(String bucketName, String placeId, byte[] imageData) {
+        if (imageData == null) return "";
+        String fileName = placeId + ".jpg";
+        BlobId blobId = BlobId.of(bucketName, fileName);
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
         try {
-            storage.createFrom(blobInfo, new ByteArrayInputStream(contents));
+            storage.createFrom(blobInfo, new ByteArrayInputStream(imageData));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         //storage.createAcl(blobId, Acl.of(Acl.User.ofAllUsers(), Acl.Role.READER));
         Blob blob = storage.get(blobId);
-        String mediaLink = blob.getMediaLink();
 
-        return mediaLink;
+        return blob.getMediaLink();
     }
-  public void deleteImageByID (
-          @Value("${GCS.bucket-name}") String bucketName, String placeID) throws IOException {
+  public void deleteImageByID (@Value("${GCS.bucket-name}") String bucketName, String placeId) {
 
-        BlobId blobId = BlobId.of(bucketName, placeID);
-        Blob blob = storage.get(bucketName, placeID);
+        BlobId blobId = BlobId.of(bucketName, placeId);
+        Blob blob = storage.get(bucketName, placeId);
         if (blob == null) {
-          System.out.println("The object " + placeID + " wasn't found in " + bucketName);
-          return;
+            LOGGER.error(String.format("The object \"%s\" wasn't found in \"%s\"", placeId, bucketName));
+            return;
         }
 
         boolean deleted =  storage.delete(blobId);
-        if(deleted) {
-            System.out.println("Object " + placeID + " was deleted from " + bucketName);
+        if (deleted) {
+            LOGGER.info(String.format("Object \"%s\" was deleted from \"%s\"", placeId, bucketName));
         } else {
-            System.out.println("The object could not be deleted.");
+            LOGGER.error("The object could not be deleted.");
         }
 
   }
